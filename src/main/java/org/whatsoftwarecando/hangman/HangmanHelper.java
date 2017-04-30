70faac7495fb0949e040b97a5edf4866f37ecacc
Andreas Keilhauer <info@keilhauer.eu>
2017-04-30 22:06:23 +0200

    English translation

--- src/main/java/org/whatsoftwarecando/hangman/HangmanHelper.java
+++ src/main/java/org/whatsoftwarecando/hangman/HangmanHelper.java
@@ -2,7 +2,6 @@ package org.whatsoftwarecando.hangman;
 
 import java.util.Scanner;
 
-import org.whatsoftwarecando.hangman.strategy.ClosestToMiddleStrategy;
 import org.whatsoftwarecando.hangman.strategy.MinMaxOneStepStrategy;
 
 public class HangmanHelper {
@@ -14,13 +13,17 @@ public class HangmanHelper {
 		try (Scanner input = new Scanner(System.in)) {
 			while (true) {
 				System.out.println("===========");
-				System.out.println("Neues Spiel");
+				String wordlistFilename = argv[0];
+				String allowedCharacters = argv[1];
+				System.out.println("New Game");
+				System.out.println("File name of wordlist: "+wordlistFilename);
+				System.out.println("Allowed characters for guesses: "+allowedCharacters);
 				System.out.println("===========");
-				einSpiel(input, argv[0], argv[1]);
-				System.out.println("Nochmal spielen? [j] für ja");
+				einSpiel(input, wordlistFilename, allowedCharacters);
+				System.out.println("Play again? [y] for yes");
 
 				String nochmal = input.nextLine();
-				if (!nochmal.equals("j")) {
+				if (!nochmal.equals("y")) {
 					break;
 				}
 				System.out.println();
@@ -30,33 +33,33 @@ public class HangmanHelper {
 
 	private static void einSpiel(Scanner input, String wordlistFilename, String allowedCharactersForGuesses) {
 
-		System.out.println("Wie viele Buchstaben?");
+		System.out.println("How many characters?");
 		Integer anzahlBuchstaben = null;
 		while (anzahlBuchstaben == null) {
 			try {
 				anzahlBuchstaben = Integer.parseInt(input.nextLine());
 			} catch (NumberFormatException n) {
-				System.err.println("Nur eine Zahl erlaubt!");
+				System.err.println("Only numbers allowed!");
 			}
 		}
-		System.out.println("Lade Wortliste für " + anzahlBuchstaben + " Buchstaben...");
+		System.out.println("Loading word list with " + anzahlBuchstaben + " letters...");
 		Wordlist wordlist = new Wordlist(HangmanHelper.class.getResourceAsStream(wordlistFilename),
 				anzahlBuchstaben);
 		
 		HangmanGame hangmanGame = new HangmanGame(wordlist, allowedCharactersForGuesses, new MinMaxOneStepStrategy());
 		System.out.println(
-				wordlist.getRemainingWords().size() + " Wörter mit " + anzahlBuchstaben + " Buchstaben geladen.");
-		System.out.println("Bester Buchstabe: " + hangmanGame.bestGuess());
+				wordlist.getRemainingWords().size() + " Words with " + anzahlBuchstaben + " letters loaded.");
+		System.out.println("Best guess: " + hangmanGame.bestGuess());
 		String nextInput = null;
 		int versuche = 1;
-		System.out.println("Versuch " + versuche);
+		System.out.println("Guess " + versuche);
 		while (!(nextInput = input.nextLine()).equals(".")) {
 			Character currentChar = null;
 			int[] places = null;
 			try {
 				String[] parts = nextInput.split(" ");
 				if (parts[0].length() != 1) {
-					throw new RuntimeException("Einen Buchstaben eingeben!");
+					throw new RuntimeException("Please enter a character followed by the positions where they occur. '.' to end the current game).");
 				}
 				currentChar = new Character(parts[0].charAt(0));
 				try {
@@ -65,7 +68,7 @@ public class HangmanHelper {
 						places[i] = Integer.parseInt(parts[i + 1]);
 					}
 				} catch (NumberFormatException n) {
-					throw new RuntimeException("Nur Zahlen erlaubt!");
+					throw new RuntimeException("Only numbers allowed!");
 				}
 			} catch (RuntimeException r) {
 				System.err.println(r.getMessage());
@@ -77,31 +80,31 @@ public class HangmanHelper {
 				if (wordlist.getRemainingWords().size() == 1) {
 					System.out.println("======================================");
 					System.out.println(
-							"Lösung gefunden nach " + versuche + " Versuchen: " + wordlist.getRemainingWords().get(0));
+							"Solution found after " + versuche + " guesses: " + wordlist.getRemainingWords().get(0));
 					System.out.println("======================================");
 				} else {
-					System.out.println("======================================");
-					System.out.println("Die Lösung ist nicht in der Wortliste.");
-					System.out.println("======================================");
+					System.out.println("=============================================");
+					System.out.println("The solution is not contained in the wordlist");
+					System.out.println("=============================================");
 				}
 				break;
 			}
 			Character bestGuess = hangmanGame.bestGuess();
 			if (bestGuess == null) {
-				System.out.println("Keine unterscheidenden erlaubten Buchstaben mehr übrig. Du musst raten:\n"
+				System.out.println("No more differentiating allowed characters left. You have to guess:\n"
 						+ wordlist.getRemainingWords());
 			} else {
 				System.out.println(wordlist.getRemainingWords().size() + " verbleibende Wörter: "
 						+ ((wordlist.getRemainingWords().size() > 20)
 								? wordlist.getRemainingWords().subList(0, 20) + " ... "
 								: wordlist.getRemainingWords()));
-				System.out.println("Bester Buchstabe: " + bestGuess);
+				System.out.println("Best guess: " + bestGuess);
 			}
 			versuche++;
-			System.out.println("Versuch " + versuche);
+			System.out.println("Guess " + versuche);
 		}
 		if (nextInput.equals(".")) {
-			System.out.println("Folgende Wörter sind übrig: " + wordlist.getRemainingWords());
+			System.out.println("The following words are left: " + wordlist.getRemainingWords());
 		}
 	}
 }
